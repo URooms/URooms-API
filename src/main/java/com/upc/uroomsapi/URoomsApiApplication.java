e739027fd7840bd8fa7cfefc89f71129e35e47c4
Jamutaq Ortega <jamutaqortegavelez@gmail.com>
2022-10-19 11:19:44 -0500

    add: add database & environment vars

--- src/main/java/com/upc/uroomsapi/URoomsApiApplication.java
+++ src/main/java/com/upc/uroomsapi/URoomsApiApplication.java
@@ -3,12 +3,19 @@ package com.upc.uroomsapi;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class URoomsApiApplication {
+public class URoomsApiApplication  implements CommandLineRunner {
+    //LOGGER
+    private static final Logger Log = LoggerFactory.getLogger(URoomsApiApplication.class);
+
     //BEANS
     @Bean
     public ModelMapper modelMapper() {
@@ -23,7 +30,17 @@ public class URoomsApiApplication {
         );
     }
 
+    //METODO MAIN
     public static void main(String[] args) {
         SpringApplication.run(URoomsApiApplication.class, args);
+    }
+
+    //VERIFICAR EL ENTORNO DE EJECUCION (DEV o PROD)
+    @Value(value = "${environment}")
+    String env;
+
+    @Override
+    public void run(String... arg0) {
+        Log.warn("************ ENV: {} ************", env);
     }
 }
